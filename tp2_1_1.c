e7adc8237d6ea7e620d9e7e17a6cd1275042bbe2
Peralta Enzo <enzash.k@gmail.com>
2023-03-25 21:48:09 -0300

    Primer comit

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+void main(){
+    
+    int i;
+    double vt[N];
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf("vt[%d]=%.2f\n",i+1,vt[i]);
+    }
+    
+}
